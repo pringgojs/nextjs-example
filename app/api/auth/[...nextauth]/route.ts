b5c4e619a1856e486df618a506983d5ad592e0b2
Pringgo Juni S <odyinggo@gmail.com>
2026-03-26 15:03:19 +0700

    feat: add Keycloak authentication and backend call functionality

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,19 @@ const handler = NextAuth({
       issuer: process.env.KEYCLOAK_ISSUER,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,18 @@ import { signIn, signOut, useSession } from "next-auth/react";
 export default function Home() {
   const { data: session } = useSession();
 
+  async function callBackend() {
+    const res = await fetch("http://127.0.0.1:8000/api/v1/protected", {
+      headers: {
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
+    alert(JSON.stringify(data, null, 2));
+  }
+
   if (!session) {
     return (
       <button onClick={() => signIn("keycloak")}>Login dengan Keycloak</button>
@@ -15,6 +27,7 @@ export default function Home() {
     <>
       <p>Login sebagai {session.user?.name}</p>
       <button onClick={() => signOut()}>Logout</button>
+      <button onClick={callBackend}>Call Backend</button>
     </>
   );
 }
